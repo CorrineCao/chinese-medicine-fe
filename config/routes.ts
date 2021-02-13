c4d4f78b9ca7cbf85deb33ebd26e7f4c09bff033
caochen <caochen@lianjia.com>
2021-02-13 18:01:08 +0800

    初步环境搭建

---
+++ config/routes.ts
@@ -0,0 +1,52 @@
+﻿export default [
+  {
+    path: '/user',
+    layout: false,
+    routes: [
+      {
+        path: '/user',
+        routes: [
+          {
+            name: 'login',
+            path: '/user/login',
+            component: './User/login',
+          },
+        ],
+      },
+    ],
+  },
+ {
+    path: '/list',
+    name: 'list',
+    icon: 'crown',
+    // access: 'canAdmin',
+    // component: './Admin',
+    routes: [
+      {
+        path: '/list/user',
+        name: 'user-list',
+        icon: 'smile',
+        component: './TableList',
+      },
+      {
+        path: '/list/auth',
+        name: 'auth-list',
+        icon: 'smile',
+        component: './TableList',
+      },
+      {
+        path: '/list/role',
+        name: 'role-list',
+        icon: 'smile',
+        component: './TableList',
+      },
+    ],
+  },
+  {
+    path: '/',
+    redirect: '/list/user',
+  },
+  {
+    component: './404',
+  },
+];
